64da332d3c7e0ebd29fce744f3d28b080a1a515f
Mateus Venancio <mateusguitar1@gmail.com>
2025-09-14 17:47:31 -0300

    att full withdraw

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -613,6 +613,74 @@ class WebhookController extends Controller
 
     }
 
+    public function suitpayWebhookWithdraw(Request $request){
+
+        $FunctionsController = new FunctionsController();
+
+        $payload = json_decode($request->getContent(),true);
+
+        $path_name = "get-webhook-withdraw-suitpay-".date("Y-m-d");
+
+        if (!file_exists('/var/www/html/nexapay/logs/'.$path_name)) {
+            mkdir('/var/www/html/nexapay/logs/'.$path_name, 0777, true);
+        }
+
+        $FunctionsController->registerRecivedsRequests("/var/www/html/nexapay/logs/".$path_name."/log.txt",json_encode($payload));
+
+        // return response()->json(["status" => "success", "message" => "received"]);
+
+        if(isset($payload['idTransaction'])){
+            $transaction = Transactions::where("status","pending")->where("payment_id","=",$payload['idTransaction'])->first();
+
+            if(!empty($transaction)){
+
+                $user_data = json_decode(base64_decode($transaction->user_account_data),true);
+
+                // if($transaction->client_id != "17"){
+                $client = Clients::where("id",$transaction->client_id)->first();
+
+                if($transaction->method_transaction == "pix"){
+
+                    if($transaction->status != "confirmed"){
+
+                        DB::beginTransaction();
+                        try{
+
+                            $webhook = Webhook::create([
+                                "client_id" => $client->id,
+                                "order_id" => $transaction->order_id,
+                                "type_register" => "pix",
+                                "body" => json_encode($payload,true),
+                            ]);
+
+                            DB::commit();
+
+                            $transaction_id = $transaction->id;
+                            $webhook_id = $webhook->id;
+
+                            \App\Jobs\CheckHookPIXSuitPayWithdraw::dispatch($transaction_id,$webhook_id)->delay(now("5"));
+
+                            return response()->json(["message" => "success"]);
+
+                        }catch(exception $e){
+                            DB::rollback();
+                        }
+
+                    }
+
+                }else{
+                    return response()->json(["message" => "method not found"]);
+                }
+
+            }else{
+                return response()->json(["message" => "transaction not found"]);
+            }
+        }else{
+            return response()->json(["message" => "out other conditions", "content" => "dont have conciliationId"]);
+        }
+
+    }
+
     public function hubapihook(Request $request){
 
         $FunctionsController = new FunctionsController();
